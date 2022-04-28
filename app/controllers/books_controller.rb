326bbf0047c034a9faee9904fa2440b56e409b3a
kao126 <76607249+kao126@users.noreply.github.com>
2022-04-28 08:05:44 +0000

    [Add] 応用課題3　コメント機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,6 +21,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @user = @book.user
     @book_new = Book.new
+    @comment = BookComment.new
   end
 
   def edit
