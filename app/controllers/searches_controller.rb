8dcbe8ce4d4912a3a1ac4417a14413665b447e58
kao126 <76607249+kao126@users.noreply.github.com>
2022-05-02 12:32:39 +0000

    [Add] 応用課題5　検索機能

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,11 @@
+class SearchesController < ApplicationController
+  def search
+    @range = params[:range]
+
+    if @range == "User"
+      @users = User.search(params[:search], params[:word])
+    else
+      @books = Book.search(params[:search], params[:word])
+    end
+  end
+end
